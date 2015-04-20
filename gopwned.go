12806e5b66ed0fb0683fc1eae0f53a8d369718b6
Maverick <mavjs01@gmail.com>
2015-04-20 17:13:38 +0200

    initial function features
    initial function now "actually" supports having a specific domain to get
    all breaches about an account.
    
    Signed-off-by: Maverick <mavjs01@gmail.com>

--- gopwned.go
+++ gopwned.go
@@ -9,12 +9,16 @@ import (
 )
 
 type jsonRespEmail struct {
-	Title      string
-	Name       string
-	Domain     string
-	BreachDate string
-	PwnCount   int
-	IsVerified bool
+	Title       string
+	Name        string
+	Domain      string
+	BreachDate  string
+	AddedDate   string
+	PwnCount    int
+	Description string
+	DataClasses []string
+	IsVerified  bool
+	LogoType    string
 }
 
 var BASEAPIURL = "https://haveibeenpwned.com/api/v2/"
@@ -39,6 +43,40 @@ func GetAllBreachesForAccount(email, domain string) string {
 
 	if domain == "" {
 		var URL = BASEAPIURL + URLENDPOINT + email
+
+		// request http api
+		req, err := http.NewRequest("Get", URL, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// set haveibeenpwned content negotiation header
+		req.Header.Add("Accept", "application/vnd.haveibeenpwned.v2+json")
+
+		// make the request
+		res, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// read body
+		body, err := ioutil.ReadAll(res.Body)
+		defer res.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		statuscode := StatusCodeCheck(res.StatusCode)
+		if statuscode != "" {
+			return fmt.Sprintf("%s", statuscode)
+		}
+
+		err = json.Unmarshal(body, &result)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		var URL = BASEAPIURL + URLENDPOINT + email + "?domain=" + domain
 
 		// request http api
 		req, err := http.NewRequest("Get", URL, nil)
