60da6d0973479c92bb6db044fd7ab6454c602041
Maverick <mavjs01@gmail.com>
2015-04-21 00:38:24 +0200

    implement all functions
    implemented all functions to query the HIBP API
    
    Signed-off-by: Maverick <mavjs01@gmail.com>

--- gopwned.go
+++ gopwned.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 )
 
-type jsonRespEmail struct {
+type jsonResp struct {
 	Title       string
 	Name        string
 	Domain      string
@@ -21,94 +21,226 @@ type jsonRespEmail struct {
 	LogoType    string
 }
 
+type jsonPasteResp struct {
+	Source     string
+	Id         string
+	Title      string
+	Date       string
+	EmailCount int
+}
+
 var BASEAPIURL = "https://haveibeenpwned.com/api/v2/"
 
-var respcodes = map[int]string{
-	400: "Bad request — the account does not comply with an acceptable format (i.e. it's an empty string)",
-	403: "Forbidden — no user agent has been specified in the request",
-	404: "Not found — the account could not be found and has therefore not been pwned",
-}
-
-func StatusCodeCheck(statuscode int) string {
-	// return status codes and exit
-	return respcodes[statuscode]
-}
-
-func GetAllBreachesForAccount(email, domain string) string {
-	URLENDPOINT := "breachedAccount/"
+func RestReq(url string) ([]byte, string) {
+	var respcodes = map[int]string{
+		400: "Bad request — the account does not comply with an acceptable format (i.e. it's an empty string)",
+		403: "Forbidden — no user agent has been specified in the request",
+		404: "Not found — the account could not be found and has therefore not been pwned",
+	}
 
 	// create http client
 	client := new(http.Client)
-	var result []jsonRespEmail
+
+	// request http api
+	req, err := http.NewRequest("Get", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// set haveibeenpwned content negotiation header
+	req.Header.Add("Accept", "application/vnd.haveibeenpwned.v2+json")
+	req.Header.Add("User-Agent", "gopwned (HITBP golang API client library)")
+	// make the request
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// read body
+	body, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	statuscode := respcodes[res.StatusCode]
+	return body, statuscode
+
+}
+
+func GetAllBreachesForAccount(email, domain string) string {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// URL Endpoint for getting all breached sites for an account
+	URLENDPOINT := "breachedAccount/"
+
+	var (
+		jsonres    []jsonResp
+		result     []byte
+		statuscode string
+	)
 
 	if domain == "" {
+
+		// build URL for getting breaches for an account
 		var URL = BASEAPIURL + URLENDPOINT + email
 
-		// request http api
-		req, err := http.NewRequest("Get", URL, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
+		result, statuscode = RestReq(URL)
 
-		// set haveibeenpwned content negotiation header
-		req.Header.Add("Accept", "application/vnd.haveibeenpwned.v2+json")
-
-		// make the request
-		res, err := client.Do(req)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// read body
-		body, err := ioutil.ReadAll(res.Body)
-		defer res.Body.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		statuscode := StatusCodeCheck(res.StatusCode)
 		if statuscode != "" {
 			return fmt.Sprintf("%s", statuscode)
 		}
 
-		err = json.Unmarshal(body, &result)
+		err := json.Unmarshal(result, &jsonres)
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else {
+
+		// build URL for getting breaches for an account on specific domain
 		var URL = BASEAPIURL + URLENDPOINT + email + "?domain=" + domain
 
-		// request http api
-		req, err := http.NewRequest("Get", URL, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
+		result, statuscode = RestReq(URL)
 
-		// set haveibeenpwned content negotiation header
-		req.Header.Add("Accept", "application/vnd.haveibeenpwned.v2+json")
-
-		// make the request
-		res, err := client.Do(req)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// read body
-		body, err := ioutil.ReadAll(res.Body)
-		defer res.Body.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		statuscode := StatusCodeCheck(res.StatusCode)
 		if statuscode != "" {
 			return fmt.Sprintf("%s", statuscode)
 		}
 
-		err = json.Unmarshal(body, &result)
+		err := json.Unmarshal(result, &jsonres)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	return fmt.Sprintf("%+v", result)
+	return fmt.Sprintf("%+v", jsonres)
+}
+
+func AllBreaches(domain string) string {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// URL Endpoint for getting details about all breached sites
+	URLENDPOINT := "breaches/"
+
+	var (
+		jsonres    []jsonResp
+		result     []byte
+		statuscode string
+	)
+
+	if domain == "" {
+
+		// build URL for getting details about all breached sites
+		var URL = BASEAPIURL + URLENDPOINT
+
+		result, statuscode = RestReq(URL)
+
+		if statuscode != "" {
+			return fmt.Sprintf("%s", statuscode)
+		}
+
+		err := json.Unmarshal(result, &jsonres)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+
+		// build URL for getting details about a single breached site
+		var URL = BASEAPIURL + URLENDPOINT + "?domain=" + domain
+
+		result, statuscode = RestReq(URL)
+
+		if statuscode != "" {
+			return fmt.Sprintf("%s", statuscode)
+		}
+
+		err := json.Unmarshal(result, &jsonres)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return fmt.Sprintf("%+v", jsonres)
+}
+
+func GetSingleBreachedSite(name string) string {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// URL Endpoint for getting details for a single breached site
+	URLENDPOINT := "breach/"
+
+	var (
+		jsonres    jsonResp
+		result     []byte
+		statuscode string
+	)
+
+	// build URL for getting details for a single breached site
+	var URL = BASEAPIURL + URLENDPOINT + name
+
+	result, statuscode = RestReq(URL)
+
+	if statuscode != "" {
+		return fmt.Sprintf("%s", statuscode)
+	}
+
+	err := json.Unmarshal(result, &jsonres)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("%+v", jsonres)
+}
+
+func GetAllDataClasses() string {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// URL Endpoint for getting breach data classes
+	URLENDPOINT := "dataclasses/"
+
+	var (
+		jsonres    interface{}
+		result     []byte
+		statuscode string
+	)
+
+	// build URL for getting breach data classes
+	var URL = BASEAPIURL + URLENDPOINT
+
+	result, statuscode = RestReq(URL)
+
+	if statuscode != "" {
+		return fmt.Sprintf("%s", statuscode)
+	}
+
+	err := json.Unmarshal(result, &jsonres)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("%+v", jsonres)
+}
+
+func GetAllPastesForAccount(email string) string {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// URL Endpoint for getting pastes for an account
+	URLENDPOINT := "pasteaccount/"
+
+	var (
+		jsonres    []jsonPasteResp
+		result     []byte
+		statuscode string
+	)
+
+	// build URL for getting pastes for an account
+	var URL = BASEAPIURL + URLENDPOINT + email
+
+	result, statuscode = RestReq(URL)
+
+	if statuscode != "" {
+		return fmt.Sprintf("%s", statuscode)
+	}
+
+	err := json.Unmarshal(result, &jsonres)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("%+v", jsonres)
+
 }
